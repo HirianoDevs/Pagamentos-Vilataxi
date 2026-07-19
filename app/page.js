6ff6029c771dd96b446da19581e71dcffcc08498
HirianoDevs <hirianodevs@gmail.com>
2026-07-19 02:41:08 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <h1>Backend VilaTáxi PaySuite</h1>
-      <p>API online</p>
+      <h1>VilaTaxi Pagamentos</h1>
+      <p>Backend funcionando</p>
     </main>
   );
 }
